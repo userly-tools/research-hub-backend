c6d8726a5a84b9f754a5e51a35c90db2709c7d42
rohanrajpal <rohan17089@iiitd.ac.in>
2020-09-13 16:37:37 +0530

    add cors

--- server.py
+++ server.py
@@ -4,10 +4,12 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 import os
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/userly.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
